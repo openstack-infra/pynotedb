b4783841bbd543f760a5965a0216831b7e72dfc1
Richard Ipsum <richard.ipsum@codethink.co.uk>
2017-02-28 14:14:14 +0000

    Update copyright
    This also includes a couple of whitespace fixes to satisfy the pep8 gate.
    
    Change-Id: Ic0addea87c03ab9cecd09471450bdfdd338c59fc

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pynotedb Release Notes'
-copyright = u'2016, OpenStack Foundation'
+copyright = u'2017, Codethink Ltd'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
